910c096bee58e7039ec0c42acace49cba629093d
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-11-22 18:14:05 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,10 @@ import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
 import doctorRouter from './routes/doctorRoute.js';
 import userRouter from './routes/userRoute.js';
-import path from "path";
 
 
 // app config
 const app = express();
-
-const _dirname = path.resolve();
-
 const port = process.env.PORT || 4000
 connectDB();
 connectCloudinary()
@@ -26,11 +22,6 @@ app.use(cors())
 app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRouter)
-
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
-app.get('*', (_, res)=>{
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
-})
 
 app.get('/',(req, res)=>{
     res.send('API WORKING')
